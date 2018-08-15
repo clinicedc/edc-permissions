7dfbe9eed1e4358039f40bcf5ff34febadc08259
erikvw <ew2789@gmail.com>
2018-08-15 10:26:55 -0500

    initial

--- edc_permissions/mixins.py
+++ edc_permissions/mixins.py
@@ -1,19 +1,133 @@
-from django.contrib.auth.models import Permission, User
+import sys
+
+from django.contrib.auth.models import Group, Permission, User
+from django.core.exceptions import ObjectDoesNotExist
 from edc_navbar.site_navbars import site_navbars
 
 
 class EdcPermissionsUpdaterMixin:
 
+    default_group_names = [
+        'ACCOUNT_MANAGER',
+        'AUDITOR',
+        'CLINIC',
+        'EVERYONE',
+        'LAB',
+        'PHARMACY',
+        'PII']
+    default_pii_app_labels = ['edc_locator', 'edc_registration']
+    default_pii_models = ['subjectlocator', 'registeredsubject']
+    group_names = None
+    pii_app_labels = None
+    pii_models = None
+
+    def __init__(self):
+        self.pii_app_labels.extend(self.default_pii_app_labels)
+        self.pii_app_labels = list(set(self.pii_app_labels))
+        self.pii_models.extend(self.default_pii_models)
+        self.pii_models = list(set(self.pii_models))
+        self.group_names.extend(self.default_group_names)
+        self.group_names = list(set(self.group_names))
+        self.update_groups()
+        self.update_group_permissions()
+
+    def update_groups(self, group_names=None):
+        sys.stdout.write('Adding or updating groups ...\n')
+        for name in group_names:
+            try:
+                Group.objects.get(name=name)
+            except ObjectDoesNotExist:
+                Group.objects.create(name=name)
+        Group.objects.exclude(name__in=self.group_names).delete()
+        sys.stdout.write(
+            f"  Groups are: "
+            f"{', '.join([obj.name for obj in Group.objects.all().order_by('name')])}\n")
+
+    def update_group_permissions(self, pii_app_labels=None, pii_models=None):
+        sys.stdout.write('Adding or updating permissions ...\n')
+
+        group_name = 'ACCOUNT_MANAGER'
+        sys.stdout.write(f' * adding permissions to {group_name}.\n')
+        group = Group.objects.get(name=group_name)
+        group.permissions.clear()
+        self.add_auth_permissions(group)
+
+        group_name = 'EVERYONE'
+        sys.stdout.write(f' * adding permissions to {group_name}.\n')
+        group = Group.objects.get(name=group_name)
+        group.permissions.clear()
+        self.add_userprofile_permissions_to_active(group)
+
+        group_name = 'AUDITOR'
+        sys.stdout.write(f' * adding permissions to {group_name}.\n')
+        group = Group.objects.get(name=group_name)
+        for permission in Permission.objects.filter(
+                content_type__app_label__in=[
+                    'ambition_ae',
+                    'ambition_prn',
+                    'ambition_screening',
+                    'ambition_subject',
+                    'edc_lab'
+                    'edc_offstudy',
+                ],
+                content_type__model__startswith='view'):
+            group.permissions.add(permission)
+        self.add_edc_action_permissions(group)
+        self.add_edc_appointment_permissions(group)
+        self.add_pii_permissions(
+            group,
+            app_labels=pii_app_labels,
+            pii_models=self.pii_models,
+            view_only=True)
+        self.add_navbar_permissions(
+            group, codenames=['default_administration', 'edc_lab', 'lab_requisition'])
+
+        group_name = 'PII'
+        sys.stdout.write(f' * adding permissions to {group_name}.\n')
+        group = Group.objects.get(name=group_name)
+        group.permissions.clear()
+        self.add_pii_permissions(
+            group,
+            app_labels=pii_app_labels,
+            pii_models=pii_models)
+
+        group_name = 'LAB'
+        sys.stdout.write(f' * adding permissions to {group_name}.\n')
+        group = Group.objects.get(name=group_name)
+        group.permissions.clear()
+        self.add_lab_permissions(group)
+
+        group_name = 'PHARMACY'
+        sys.stdout.write(f' * adding permissions to {group_name}.\n')
+        group = Group.objects.get(name=group_name)
+        group.permissions.clear()
+        self.add_pharmacy_permissions(group)
+
+    def update_clinic_group_permissions(self, group):
+        """Override for custom group permissions.
+        """
+        pass
+
+    def _update_clinic_group_permissions(self):
+        group_name = 'CLINIC'
+        sys.stdout.write(f' * adding permissions to {group_name}.\n')
+        group = Group.objects.get(name=group_name)
+        group.permissions.clear()
+        self.update_clinic_group_permissions(group)
+        self.add_edc_appointment_permissions(group)
+        self.add_edc_action_permissions(group)
+        self.remove_historical_permissions(group)
+        self.add_navbar_permissions(
+            group, codenames=[
+                'default_administration', 'edc_lab', 'lab_requisition'])
+
     def remove_historical_permissions(self, group):
         group.permissions.filter(codename__contains='historical').exclude(
             codename__startswith='view').delete()
 
-    def add_pii_permissions(self, group, app_labels=None, view_only=None):
-        app_labels.append('edc_locator')
-        app_labels.append('edc_registration')
-        app_labels = list(set(app_labels))
+    def add_pii_permissions(self, group, view_only=None):
         lookup = dict(
-            content_type__app_label__in=app_labels,
+            content_type__app_label__in=self.pii_app_labels,
             content_type__model__in=self.pii_models)
         if view_only:
             lookup.update(codename__startswith='view')
